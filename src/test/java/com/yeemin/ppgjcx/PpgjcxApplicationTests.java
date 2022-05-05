c204e6fadaee9f092aebb31ebe2ce5ebf2de882b
yeeminshon <yeeminshon@outlook.com>
2022-05-05 10:51:58 +0800

    first commit

---
+++ src/test/java/com/yeemin/ppgjcx/PpgjcxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeemin.ppgjcx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpgjcxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
