8b20f0ff79fdd50257b2486cf650cabbc447d83b
yeeminshon <yeeminshon@outlook.com>
2022-05-05 15:01:43 +0800

    完善maven依赖

--- src/main/java/com/yeemin/ppgjcx/PpgjcxApplication.java
+++ src/main/java/com/yeemin/ppgjcx/PpgjcxApplication.java
@@ -1,27 +1,13 @@
 package com.yeemin.ppgjcx;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class PpgjcxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PpgjcxApplication.class, args);
-	}
-
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-
-	@Bean
-	public ApplicationRunner initDatabase() {
-		return args -> {
-
-		};
 	}
 
 }
