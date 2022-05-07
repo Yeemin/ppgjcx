4e4fb68b34516fc9ddab3864f2278634ffda1812
yeeminshon <yeeminshon@outlook.com>
2022-05-07 15:13:05 +0800

    精简一下

--- src/main/java/com/yeemin/ppgjcx/controller/DemoController.java
+++ src/main/java/com/yeemin/ppgjcx/controller/DemoController.java
@@ -9,9 +9,6 @@ import com.yeemin.ppgjcx.entity.User;
 import com.yeemin.ppgjcx.service.DemoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,30 +22,20 @@ public class DemoController {
     private LuceneOperation luceneOperation;
 
     @Autowired
-    CacheManager cacheManager;
-
-    @Autowired
     private DemoService demoService;
 
     @RequestMapping("/add")
     public Map<String, Object> add(@RequestBody User user) {
         String id = UUID.randomUUID().toString();
+        user.setId(id);
         luceneOperation.index(UUID.randomUUID().toString(), user);
-        Cache cache = cacheManager.getCache("demo");
-        System.out.println(cacheManager.getCacheNames());
-        cache.put(id, user);
+        demoService.putUser(user);
         return Collections.singletonMap("id", id);
     }
 
     @RequestMapping("/get/{id}")
     public User get(@PathVariable("id") String id) {
-        Cache cache = cacheManager.getCache("demo");
-        return cache.get(id, User.class);
-    }
-
-    @RequestMapping("getCache")
-    public User getCache() {
-        return demoService.queryUser("asdfasfd");
+        return demoService.queryUser(id);
     }
 
 }
--- src/main/java/com/yeemin/ppgjcx/service/impl/DemoServiceImpl.java
+++ src/main/java/com/yeemin/ppgjcx/service/impl/DemoServiceImpl.java
@@ -3,6 +3,7 @@ package com.yeemin.ppgjcx.service.impl;
 import com.yeemin.ppgjcx.entity.User;
 import com.yeemin.ppgjcx.service.DemoService;
 
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,12 @@ public class DemoServiceImpl implements DemoService {
         user.setUsername("123123");
         user.setAge(28);
         System.out.println("query user");
+        return user;
+    }
+
+    @Override
+    @CachePut(value = "demo", key = "'demo.user' + #id")
+    public User putUser(User user) {
         return user;
     }
     
